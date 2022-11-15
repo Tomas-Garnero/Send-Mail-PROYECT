3813ad4777bea2201a5c200282670f9e08aeb2f3
unknown <tomygarnero9429@gmail.com>
2022-11-15 18:09:44 -0300

    Mostrar error

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Variables
 const btnEnviar = document.querySelector("#enviar");
+const formulario = document.querySelector("#enviar-mail");
 
 // Variables para campos
 const email = document.querySelector("#email");
 const asunto = document.querySelector("#asunto");
 const mensaje = document.querySelector("#mensaje");
+
 
 eventListeners();
 function eventListeners() {
@@ -29,5 +31,18 @@ function validarFormulario(e) {
         console.log("Si hay algo");
     } else {
         e.target.classList.add("border", "border-red-500");
+
+        mostrarError();
+    }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement("p");
+    mensajeError.textContent = "Todos los campos son obligatorios";
+    mensajeError.classList.add("border", "border-red-500", "background-red-100", "text-redd-500", "p-3", "mt-5", "text-center", "error");
+
+    const errores = document.querySelectorAll(".error");
+    if (errores.length === 0) {
+        formulario.insert(mensajeError);
     }
 }
